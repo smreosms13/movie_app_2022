5a110a48eae10e218884ac487441b1efd98605a5
smreosms13 <smreosms13@gamil.com>
2022-08-02 15:30:52 +0900

    03-3 JSX 개념알아보기 - Potato 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import React from "react";
+
+function Potato () {
+  return <h3>I love Potato</h3>;
+}
 
 
 function App() {
   //return <div className="App"/>;
-  return <div>Hello!!!!</div>
+  return (
+  <div>
+    <h1>Hello</h1>
+    <Potato />
+  </div>
+  );
 }
 
 export default App;
