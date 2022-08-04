7e38f0f81ca6f90fce5bed2a1fe86c939d4be13b
smreosms13 <smreosms13@gamil.com>
2022-08-04 16:16:40 +0900

    05-3 클래스형 컴포넌트의 생명주기 함수

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@
 import React from "react";
 
 class App extends React.Component {
+  //클래스형 컴포넌트가 호출될 때 실행
+  constructor(props) {
+    super(props);
+    console.log('hello')
+  }
+
   state = {
     count : 0,
   };
   
+  //setState() : 바뀐 state의 데이터를 업데이트
   add = () => {
     this.setState(current => (
       {count : current.count +1 }
@@ -18,7 +25,24 @@ class App extends React.Component {
     ))
   };
 
+  //컴포넌트가 처음 화면에 그러질 때 실행 : Mount
+  componentDidMount() {
+    console.log('component rendered')
+  }
+
+  //화면이 업데이트(새로 그려질 때)할 때 실행 : Update
+  componentDidUpdate() {
+    console.log('I just updated')
+  }
+
+  //컴포넌트가 죽을 때, 보통 컴포넌트에 적용한 이벤트 리스너를 제거할 때 많이 사용, :Unmount
+  componentWillUnmount() {
+    console.log('Goodbye, cruel world')
+  }
+
+  //화면 업데이트
   render() {
+    console.log('I am rendering');
     return(
       <div>
         <h1> The number is : {this.state.count}</h1>
