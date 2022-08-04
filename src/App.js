f64bfeb1aac2b1d3e60bca7419301a9eb5b33436
smreosms13 <smreosms13@gamil.com>
2022-08-04 22:19:56 +0900

    06-1 영화 api 사용 + async/await

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
     isLoading : true,
     movies : []
   }
-
+  componentDidMount () {
+    //영화 데이터 로딩
+    axios.get('https://yts-proxy.now.sh/list_movies.json');
+  }
   render() {
-
     const {isLoading} = this.state;
     return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
   }
   
-  componentDidMount () {
-    //영화 데이터 로딩
-    setTimeout(() => {
-      this.setState({isLoading : false});
-    }, 6000) 
-  }
+  
 }
 export default App;
