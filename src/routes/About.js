68f8bcbc2d415e6195e97f7bdb4143be676ab6f5
smreosms13 <smreosms13@gamil.com>
2022-08-05 17:51:57 +0900

    About 페이지 내용 구성

--- src/routes/About.js
+++ src/routes/About.js
@@ -2,7 +2,18 @@ import React from "react";
 import "./About.css"
 
 function About (){
-    return <span>About this page : I built it because I love movies.</span>;
+    return (
+        <div className="about__container">
+            <span>
+            "The best love is the kind that awakens the soul and makes us reach for more, 
+            that plants a fire in our hearts and brings peace to our minds. 
+            And that's what you've given me. 
+            
+            That's what I'd hoped to give you forever"
+            </span>
+            <span>Nicholas, The Notebook.</span>
+        </div>
+    );
 }
 
 export default About;
