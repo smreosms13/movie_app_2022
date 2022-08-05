bd3f535ce3608c54694e2d453b0ec02ba640cd30
smreosms13 <smreosms13@gamil.com>
2022-08-05 17:31:25 +0900

    08 라우터 생성 + Home,About 페이지 만들기

--- src/App.js
+++ src/App.js
@@ -1,54 +1,18 @@
 
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
 import "./App.css";
+import {HashRouter, Routes, Route} from "react-router-dom";
+import About from './routes/About';
+import Home from './routes/Home';
 
-class App extends React.Component {
-  state = {
-    isLoading : true,
-    movies : []
-  }
-  getMovies = async () => {
-    //구조 분해 할당
-    const {
-      data: {
-        data : {movies},
-      },
-    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
-    //movie state 변경 -> render() 실행
-    //key ==변수 -> 변수 이름으로 축약 가능
-    //영화 데이터를 가져오고 isLoading 업데이트
-    this.setState({movies, isLoading:false });      
-  }
-  componentDidMount() {
-    this.getMovies();
-  }
-  render() {
-    const {isLoading, movies} = this.state;
-    return (
-      <section className="container">{isLoading ? (
-        <div className ="loader">
-          <span className ="loader__text">'Loading...' </span>
-        </div>)
-        : (<div className = "movies">
-            {movies.map((movie)=> (
-              <Movie 
-                key = {movie.id}
-                id = {movie.id} 
-                year={movie.year} 
-                title = {movie.title}
-                summary = {movie.summary}
-                poster = {movie.medium_cover_image}
-                genres = {movie.genres}
-              />
-          ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" exact={true} element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </HashRouter>
+  )
 }
-
-
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,54 @@
+
+import React from "react";
+import axios from "axios";
+import Movie from "../components/Movie";
+import "./Home.css";
+
+class Home extends React.Component {
+  state = {
+    isLoading : true,
+    movies : []
+  }
+  getMovies = async () => {
+    //구조 분해 할당
+    const {
+      data: {
+        data : {movies},
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    //movie state 변경 -> render() 실행
+    //key ==변수 -> 변수 이름으로 축약 가능
+    //영화 데이터를 가져오고 isLoading 업데이트
+    this.setState({movies, isLoading:false });      
+  }
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const {isLoading, movies} = this.state;
+    return (
+      <section className="container">{isLoading ? (
+        <div className ="loader">
+          <span className ="loader__text">'Loading...' </span>
+        </div>)
+        : (<div className = "movies">
+            {movies.map((movie)=> (
+              <Movie 
+                key = {movie.id}
+                id = {movie.id} 
+                year={movie.year} 
+                title = {movie.title}
+                summary = {movie.summary}
+                poster = {movie.medium_cover_image}
+                genres = {movie.genres}
+              />
+          ))}
+          </div>
+        )}
+      </section>
+    );
+  }
+}
+
+
+export default Home;
